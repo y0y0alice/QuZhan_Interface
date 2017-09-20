d6b1d89fdc049e1fe11289bf9a5e72073f28bff8
lufei <y0y0alice@hotmail.com>
2017-09-20 15:02:41 +0800

    修改

---
+++ QuartzDemo/QuartzJobs/LwJob.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzDemo.QuartzJobs
+{
+    public sealed class LwJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(LwJob));
+
+        public void Execute(IJobExecutionContext context)
+        {
+            _logger.InfoFormat("来文测试");
+        }
+    }
+}
---
+++ QuartzDemo/QuartzJobs/Test1Job.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzDemo.QuartzJobs
+{
+    public sealed class Test1Job : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(Test1Job));
+
+        public void Execute(IJobExecutionContext context)
+        {
+            _logger.InfoFormat("Test1Job测试");
+        }
+    }
+}
